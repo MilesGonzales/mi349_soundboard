085f5d0024417cd4898f7f40cbdd007498bb8ffa
MilesGonzales <gonza796@msu.edu>
2021-11-18 17:24:02 -0500

    updated all sounds

--- soundboard.js
+++ soundboard.js
@@ -1,3 +1,15 @@
 function playSnare(){
     var audio = document.getElementById("snare").play();
 }
+
+function playBass(){
+    var audio = document.getElementById("bass").play();
+}
+
+function playHiHat(){
+    var audio = document.getElementById("hiHat").play();
+}
+
+function playFloorTom(){
+    var audio = document.getElementById("floorTom").play();
+}
